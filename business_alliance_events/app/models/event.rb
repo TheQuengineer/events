a0e07a1bffc1d235055d9176f46c172aa2b292d1
Quentin Thomas <qmanthomas@gmail.com>
2014-11-30 02:10:02 -0800

    Added 3rd event plus assets, as well as created a errors partial file

--- business_alliance_events/app/models/event.rb
+++ business_alliance_events/app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
+  validates :name, :location, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+    with: /\.(png|jpg|gif)/,
+    message: "must reference a GIF, JPG, or PNG image file name"
+  }
+
 
   def self.upcoming
     where("starts_at >= ?", Time.now).order("starts_at")
